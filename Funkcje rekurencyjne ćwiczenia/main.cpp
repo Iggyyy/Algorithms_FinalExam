893be86cd89335ba439447c9b1dbbaddf8517c68
Iggyyy <ignacy.stepka@gmail.com>
2019-09-12 22:58:28 +0200

    recursive practice folder

---
+++ Funkcje rekurencyjne ćwiczenia/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+long long factorial_recursion(long long base)
+{
+    if(base !=1) return base*factorial_recursion(base-1);
+    else
+        return base;
+}
+
+long long power_recursive(long long base, long long upto)
+{
+    if(upto == 0) return 1;
+    else{
+        return base*power_recursive(base, upto-1);
+    }
+
+}
+
+
+
+int main()
+{
+    cout<<"Factorial: "<<endl;
+    int a;
+    cin>>a;
+    cout<<factorial_recursion(a)<< '\n';
+
+    cout<<"Power (base and power index): "<<endl;
+    int b, p;
+    cin>>b>>p;
+    cout<<power_recursive(b,p);
+
+    return 0;
+}
