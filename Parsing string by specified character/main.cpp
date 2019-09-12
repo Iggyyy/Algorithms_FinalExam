5e7ca85aa346dc197b5d0fa94bce2691ce1d9c53
Iggyyy <ignacy.stepka@gmail.com>
2019-09-12 09:18:15 +0200

    parsing string completed

--- Parsing string by specified character/main.cpp
+++ Parsing string by specified character/main.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
-
+#include <vector>
+#include <string>
 using namespace std;
+
+void parseByChar(string str, char ch, vector<string> &vect)
+{
+
+    while(!str.empty())
+    {
+        if(str.find(ch) != string::npos)
+        {
+            int dig = str.find(ch);
+            vect.push_back(str.substr(0, dig));
+            str.erase(0, dig+1);
+
+            cout << dig <<endl;
+        }
+        else
+        {
+            cout << str <<endl;
+            vect.push_back(str);
+            str.erase(0, str.length());
+        }
+    }
+
+}
+
+
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    vector<string> parsed;
+    string s;
+    char a;
+    cout<<"string, char"<<endl;
+    cin>>s>>a;
+    parseByChar(s,a,parsed);
+
+
+    for(auto i : parsed)
+        cout<<i<<endl;
+
     return 0;
 }
