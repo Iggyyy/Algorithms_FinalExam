a119004d1a4c9e91286871089485a01a5989baad
Iggyyy <ignacy.stepka@gmail.com>
2019-09-17 17:01:47 +0200

    nww i nwd recursive and optimized euclides

--- Nww i nwd/main.cpp
+++ Nww i nwd/main.cpp
@@ -5,27 +5,49 @@ using namespace std;
 
 int nwd(int a, int b)
 {
-    cout<<a<<" "<<b<<endl;
-   if (a > b) a = nwd(a-b, b);
-   else if (b>a) b = nwd(a, b-a);
-   else
+   while(a != b)
    {
-       return a;
-       cout<<a<<endl;
+       if (a>b) a-=b;
+       else b-=a;
    }
-cout<<a<<" - "<<b<<endl;
-
      return a;
+}
+int nww(int a, int b)
+{
+    return (a*b)/nwd(a,b);
+}
+
+int NWDrecurs(int a, int b)
+{
+    if(a > b) a = NWDrecurs(a-b, b);
+    else if (a <b) a = NWDrecurs(a, b-a);
+    else return a;
+}
+
+int NWDoptimized(int a, int b)
+{
+    while(b != 0)
+    {
+      int c = b;
+      b = a % b;
+      a = c;
+    }
+    return a;
 }
 
 
 int main()
 {
 
-    cout<<"NWW i NWD\nProszê podaæ a i b: "<<endl;
+    cout<<"NWW i NWD\nProsze podac a i b: "<<endl;
     int a,b;
     cin>>a>>b;
     cout<<"nwd: "<<nwd(a,b)<<endl;
+    cout<<"nww: "<<nww(a,b)<<endl;
+    cout<<"nwdRecursive: "<<NWDrecurs(a,b)<<endl;
+    cout<<"nwdOptimized: "<<NWDoptimized(a,b)<<endl;
+
+
 
     return 0;
 }
