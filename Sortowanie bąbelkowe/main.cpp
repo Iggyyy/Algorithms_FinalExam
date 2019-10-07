6e423d823e84efd60f0ad28b7c1162d72ed6e236
Iggyyy <ignacy.stepka@gmail.com>
2019-10-07 17:06:46 +0200

    sort practice

--- Sortowanie bąbelkowe/main.cpp
+++ Sortowanie bąbelkowe/main.cpp
@@ -35,12 +35,32 @@ void bubble_sort_descending(vector<int> tablica)
         cout<< i <<endl;
 }
 
+
+void bubble_practice(vector<int> tab)
+{
+
+    for(int i = 0; i< tab.size(); i++)
+    {
+
+        for(int j = 0; j < tab.size()- i-1; j++)
+        {
+            if(tab[j] > tab[j+1])
+                swap(tab[j], tab[j+1]);
+        }
+
+    }
+
+    for(auto i : tab)
+        cout<<i<<endl;
+}
+
 int main()
 {
     vector<int> tablica {1 ,2, 5, 5, 7, 1, 2, 19,3};
 
     bubble_sort_ascending(tablica);
     bubble_sort_descending(tablica);
+    bubble_practice(tablica);
 
 
 
